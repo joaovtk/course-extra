8ee53e23477d91e523ce00f3b28b1b16b643cbdf
joaovtk <joaovitorsoaresdosreis3@gmail.com>
2024-04-24 15:02:24 -0300

    Convertendo Strings para float ou int

--- js/README.md
+++ js/README.md
@@ -1,4 +1,9 @@
 # Atividades 24/05/2024 PROZ
+
+parte 01
 - Commit 1: Hello World Basico
 - Commit 2: Debug e Variaveis
 - Commit 3: Uso e Condiçoes e Operadores Logicos
+
+
+parte 02
---
+++ js/parte02/main.js
@@ -0,0 +1,6 @@
+let n1 = Number.parseFloat(prompt("Digite um numero: "));
+let n2 = Number.parseFloat(prompt("Digite outro numero: "));
+
+let n3 = n1 + n2;
+
+alert("O Resultado é: "+ n3);
