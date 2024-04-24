ee4bb08ff51f56d29fba0dcabf2c4b17674064db
joaovtk <joaovitorsoaresdosreis3@gmail.com>
2024-04-24 15:15:03 -0300

    Adicionando 4 operaçoes basicas

--- js/parte02/main.js
+++ js/parte02/main.js
@@ -1,12 +1,21 @@
 let n1 = Number.parseFloat(prompt("Digite um numero: "));
 let n2 = Number.parseFloat(prompt("Digite outro numero: "));
 
-let op = prompt("1 - Adição\n2 - Subtração\nDigite sua Opção: ");
+let op = prompt("1 - Adição\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\nDigite sua Opção: ");
 
 if(op == 1){
     n3 = n1 + n2;
-}else {
+}else if(op == 2){
     n3 = n1 - n2;
+}else if(op == 3){
+    n3 = n1 / n2;
+}else if(op == 4){
+    n3 = n1 + n2;
 }
 
-alert("O Resultado é: "+ n3);
+if(op <= 4){
+    alert("O Resultado é: "+ n3);
+}else {
+    alert("Opção Invalida\nRecarregue a pagina para tentar de novo");
+}
+
