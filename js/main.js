fbda20178794ca027e5a3d53d164a011f203c411
joaovtk <joaovitorsoaresdosreis3@gmail.com>
2024-04-24 14:47:25 -0300

    Uso de condiçoes e operadores logicos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 // window.alert("Hello World");
 let nome = window.prompt("Digite seu nome: ");
+let hora = window.prompt("Quantas horas: ");
 
-window.alert("Boa Tarde, "+ nome);
+if(hora < 12){
+    window.alert("Bom Dia, " + nome);
+}else if(hora < 18){
+    window.alert("Boa Tarde, " + nome);
+}else {
+    window.alert("Boa Noite, " + nome);
+}
+/* 
+    == igualdade
+    === igualdade restrita
+    > maio que
+    < menor que
+    >= maior ou igual a 
+    <= menor ou igual a
+    != diferente
+    !== diferença restrita
+*/
