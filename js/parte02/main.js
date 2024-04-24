567334c87e30794bf16f2f5c698420af3d6cd5cd
joaovtk <joaovitorsoaresdosreis3@gmail.com>
2024-04-24 15:06:06 -0300

    Fazendo uma mini calculadora

--- js/parte02/main.js
+++ js/parte02/main.js
@@ -1,6 +1,12 @@
 let n1 = Number.parseFloat(prompt("Digite um numero: "));
 let n2 = Number.parseFloat(prompt("Digite outro numero: "));
 
-let n3 = n1 + n2;
+let op = prompt("1 - Adição\n2 - Subtração\nDigite sua Opção: ");
+
+if(op == 1){
+    n3 = n1 + n2;
+}else {
+    n3 = n1 - n2;
+}
 
 alert("O Resultado é: "+ n3);
