577e958aacb8076df7b534832830bea85817a265
joaovtk <joaovitorsoaresdosreis3@gmail.com>
2024-04-26 14:30:51 -0300

    Finalizando a atividade extra

--- js/extra/main.js
+++ js/extra/main.js
@@ -1,15 +1,22 @@
-let inputa = Number(document.querySelector("#a").value);
-let inputb = Number(document.querySelector("#b").value);
-let inputc = Number(document.querySelector("#c").value);
-let txt = document.querySelector("#txt");
+let txt = document.getElementById("txt")
+let form = document.querySelector("form");
 
-let button = document.querySelector("#btn");
-
-button.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let tipo;
+    let inputa = Number(document.getElementById("a").value);
+    let inputb = Number(document.getElementById("b").value);
+    let inputc = Number(document.getElementById("c").value);
     if((inputa + inputb) > inputc){
-
+        if((inputa == inputb) && (inputb == inputc)){
+            tipo = "Equilatero"
+        }else if((inputa == inputb) || (inputa == inputc) || (inputb == inputc)){
+            tipo = "Isoceles"
+        }else {
+            tipo = "Escaleno"
+        }
+        txt.textContent = `Os valores fornecidos formam um triângulo do tipo ${tipo}`;
     }else {
-        txt.textContent("")
+        txt.textContent = "Os valores fornectidos não formam um triângulo";
     }
 });
